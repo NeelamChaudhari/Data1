5c3d288eea84ab035b096b1a29e10f1920de7776
Admin <Admin@DESKTOP-B5L4A4H>
2023-03-12 17:20:36 +0530

    This is my New folder

---
+++ src/Basic/Simple_Program.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class Simple_Program
+{
+ 
+	public static void main(String[] args)
+	{
+		System.out.println("How are you");
+		m1();
+	}
+	public =-0p void m1()
+	{
+		System.out.println("I am fine");
+	}
+}
